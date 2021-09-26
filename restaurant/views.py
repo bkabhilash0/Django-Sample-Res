a1edf5079e7ea5ba65e6f2977e719c746c157700
Abhilash BK <bkabhilash0@gmail.com>
2021-09-26 22:31:59 +0530

    First Commit

---
+++ restaurant/views.py
@@ -0,0 +1,12 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'home.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
